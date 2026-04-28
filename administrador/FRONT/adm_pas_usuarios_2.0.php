916b3349556d5a449de50084eaebd0741092d665
Patricio <patrick28111997@gmail.com>
2026-04-27 23:16:03 -0500

    actualizacion del formulario de cambio de contraseña

--- administrador/FRONT/adm_pas_usuarios_2.0.php
+++ administrador/FRONT/adm_pas_usuarios_2.0.php
@@ -1,4 +1,4 @@
-<?Php 
+<?php 
 /**
  * Developer    :   Asael Tello Barcia
  * Fecha        :   04-09-2017
@@ -31,9 +31,7 @@ require_once('../../Librerias/procedimientos/almacenados_standar.php');
         {        
             $response['existe']=1;
             $response['success'] = true;
-        }
-        else
-        {
+        } else {
             $response['existe']=0;
             $response['success'] = true;
         }
@@ -66,96 +64,226 @@ require_once('../../Librerias/procedimientos/almacenados_standar.php');
 <!DOCTYPE html>
 <HTML>
     <HEAD>
-        <!--TITLE><?Php echo $Ses_Sys_Nom; ?></TITLE-->
-        <TITLE><?Php echo "Usuario Password [EXA]"; ?></TITLE>
+        <!--TITLE><?php echo $Ses_Sys_Nom; ?></TITLE-->
+        <TITLE><?php echo "Usuario Password [EXA]"; ?></TITLE>
         <meta charset= "UTF-8">
         <link rel="stylesheet" type="text/css" media="screen" href="../../framework/jquery/chosen/chosen-1.4.2/chosen.min.css" />
-        <?Php require_once("../../mascaras/model1/estilos/jqgrid5.php") ?>
+        <?php require_once("../../mascaras/model1/estilos/jqgrid5.php") ?>
         <script type="text/javascript" src="../../framework/jquery/chosen/chosen-1.4.2/chosen.min.js"></script>
         <script type="text/javascript" src="../../framework/jquery/chosen/chosenDesc/chosenDesc.js"></script>        
-        <script language="javascript" src="../VALIDACIONES/adm_val_usuarios_2.0.js"></script>
+        <script language="javascript" src="../VALIDACIONES/adm_val_usuarios_2.0.js?e=3"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.2.0/zxcvbn.js"></script>
+        <style>
+            :root {
+                --primary-blue: #337ab7;
+                --dark-blue: #2c3e50;
+                --bg-soft: #ebf2f7;
+                --card-shadow: 0 1px 4px rgba(0, 0, 0, 0.05);
+            }
+
+            body {
+                background-color: #f5f7f9;
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                color: #333;
+            }
+
+            .panel-main {
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                box-shadow: var(--card-shadow);
+                margin: 20px 10px;
+                background: white;
+            }
+
+            .exa-header {
+                background-color: var(--dark-blue) !important;
+                padding: 10px 15px !important;
+                border-bottom: 2px solid var(--primary-blue) !important;
+            }
+
+            .exa-header .panel-title {
+                color: white !important;
+                font-weight: 600;
+                font-size: 1.2rem;
+                margin: 0;
+            }
+
+            .exa-body {
+                padding: 30px !important;
+                background-color: #f9fbfd;
+            }
+
+            .exa-fieldset {
+                border: 1px solid #dce4ec;
+                padding: 25px !important;
+                margin-bottom: 25px;
+                border-radius: 4px;
+                background: white;
+                position: relative;
+            }
+
+            .Titulos2 {
+                width: auto;
+                border-bottom: none;
+                padding: 0 10px;
+                margin-bottom: 0;
+                font-size: 1rem;
+                font-weight: 700;
+                color: #2c3e50;
+                position: absolute;
+                top: -10px;
+                left: 15px;
+                background: white;
+            }
+
+            .form-group {
+                margin-bottom: 15px;
+            }
+
+            .control-label {
+                color: #555;
+                font-weight: 600;
+                padding-top: 7px;
+            }
+
+            .form-control {
+                border-radius: 4px;
+                border: 1px solid #ccc;
+                height: 34px;
+            }
+
+            .form-control:focus {
+                border-color: var(--primary-blue);
+                box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(102, 175, 233, .6);
+            }
+
+            .input-group-addon {
+                background-color: #eee;
+                border-color: #ccc;
+            }
+
+            .required:before {
+                content: "* ";
+                color: #27ae60;
+                font-weight: bold;
+            }
+
+            #btnGuardar {
+                background-color: var(--primary-blue);
+                border: 1px solid #2e6da4;
+                padding: 8px 30px;
+                border-radius: 4px;
+                font-weight: 600;
+                color: white;
+                transition: all 0.2s;
+            }
+
+            #btnGuardar:hover {
+                background-color: #286090;
+                border-color: #204d74;
+            }
+
+            .data-info-box {
+                background: #f8fafc;
+                border-radius: 4px;
+                padding: 15px;
+                margin: 10px 0 25px 0;
+                border: 1px solid #e2e8f0;
+            }
+
+            .data-label-text {
+                font-weight: 700;
+                color: #2c3e50;
+            }
+
+            #password-strength-meter {
+                width: 100%;
+                height: 6px;
+                margin-top: 5px;
+            }
+
+            #password-strength-text {
+                font-size: 11px;
+                margin-top: 4px;
+                color: #777;
+            }
+
+            @media (max-width: 768px) {
+                .exa-body {
+                    padding: 15px !important;
+                }
+            }
+        </style>
     </HEAD>
     <BODY>
-        <div class="panel panel-main">
-            <div class="panel-heading exa-header"><h3 class="panel-title">&raquo;  Cambio De Clave De Usuario</h3></div>
-            <div class="panel-body ui-widget-content ui-corner-bottom exa-body">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div id="tabsUser" class="ui-tab-fix ui-tabs">                                               
-                            <!-- CREAR TAB !-->
-                            <div id="tabs-1" style="min-height: 150px;">
-                                <div class="col-sm-3"></div>
-                                <div class="col-md-6 col-sm-8">
-                                <form class="form-horizontal normal" id="frmUser" name="frmUser" autocomplete="off">                                   
+        <div class="container-fluid">
+            <div class="panel panel-main">
+                <div class="panel-heading exa-header">
+                    <h3 class="panel-title"><i class="glyphicon glyphicon-lock"></i> Cambio de Clave de Usuario</h3>
+                </div>
+                <div class="panel-body exa-body">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+                            <form id="frmUser" name="frmUser" autocomplete="off" class="form-horizontal">                                   
+                                <fieldset class="exa-fieldset">
+                                    <legend class="Titulos2">Datos a Modificar</legend>
                                     
-                                    <!-- LOGUEO -->
-                                    <fieldset class="exa-fieldset" >
-                                        <legend class="Titulos2">Datos a Modificar</legend>
-                                        
-                                        <!-- Cedula -->
-                                        <div class="form-group">
-                                            <label class="col-xs-3 control-label label-xs">Cédula:</label>  
-                                            <div class="col-xs-4" >
-                                                <label id="Usu_Ced" name="Usu_Ced"><?Php echo $Ses_Usu_Ced; ?></label>
+                                    <div class="data-info-box">
+                                        <div class="row">
+                                            <div class="col-xs-6 text-center">
+                                                <span style="color: #666;">Cédula:</span> 
+                                                <span class="data-label-text" id="Usu_Ced"><?php echo $Ses_Usu_Ced; ?></span>
+                                            </div>
+                                            <div class="col-xs-6 text-center">
+                                                <span style="color: #666;">Usuario:</span> 
+                                                <span class="data-label-text" id="Usu_Nom"><?php echo $Ses_Prs_Ape." ".$Ses_Prs_Nom; ?></span>
                                             </div>
                                         </div>
-
-                                        <!-- Usuario -->
-                                        <div class="form-group">
-                                            <label class="col-xs-3 control-label label-xs">Usuario:</label>  
-                                            <div class="col-xs-4" >
-                                                <label id="Usu_Nom" name="Usu_Nom"><?Php echo $Ses_Prs_Ape." ".$Ses_Prs_Nom; ?></label>
-                                            </div>
-                                        </div>
-                                        
-                                        <!-- Clave -->
-                                        <div class="form-group">
-                                            <label class="col-xs-3 control-label label-xs required">Clave Actual:</label>  
-                                            <div class="col-xs-4" >
-                                                <div class="input-group input-group-xs">
-                                                    <input id="Usu_Pal_c" name="Usu_Pal_c" type="password" class="form-control input-xs" required="" />
-                                                    <span class="input-group-addon validate" ><i></i></span>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <!-- Nueva Clave -->
-                                        <div class="form-group">
-                                            <label class="col-xs-3 control-label label-xs required">Nueva Clave:</label>  
-                                            <div class="col-xs-4" >
-                                                <div class="input-group input-group-xs">
-                                                    <input id="Usu_Pal" name="Usu_Pal" type="password" class="form-control input-xs" required="" />
-                                                    <span class="input-group-addon validate" ><i></i></span>                                                    
-                                                </div>                                                
-                                            </div>
-                                            <div class="col-xs-2">
-                                                <meter max="4" id="password-strength-meter"></meter>
-                                            </div>
-                                            <div>
-                                                <p id="password-strength-text"></p>
-                                            </div>
-                                        </div>
-
-                                        <!-- Confirmar Clave -->
-                                        <div class="form-group">
-                                            <label class="col-xs-3 control-label label-xs required">Confirmar Clave:</label>  
-                                            <div class="col-xs-4" >
-                                                <div class="input-group input-group-xs">
-                                                    <input id="Usu_Pal_C" name="Usu_Pal_C" type="password" class="form-control input-xs" required="" />
-                                                    <span class="input-group-addon validate" ><i></i></span>
-                                                </div>
-                                            </div>
-                                        </div>                                       
-
-                                    </fieldset>
-                                    
-                                    <div class="center">
-                                        <button type="button" class="btn btn-sm btn-primary no" id="btnGuardar"><i class="glyphicon glyphicon-floppy-disk"></i> Guardar</button>
                                     </div>
-                                </form>
-                            </div>
-                            </div>
+
+                                    <!-- Clave Actual -->
+                                    <div class="form-group">
+                                        <label class="col-sm-4 control-label required">Clave Actual:</label>  
+                                        <div class="col-sm-6">
+                                            <div class="input-group">
+                                                <input id="Usu_Pal_c" name="Usu_Pal_c" type="password" class="form-control" required />
+                                                <span class="input-group-addon validate"><i></i></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Nueva Clave -->
+                                    <div class="form-group">
+                                        <label class="col-sm-4 control-label required">Nueva Clave:</label>  
+                                        <div class="col-sm-6">
+                                            <div class="input-group">
+                                                <input id="Usu_Pal" name="Usu_Pal" type="password" class="form-control" placeholder="Mínimo 8 caracteres (letras y números)" required />
+                                                <span class="input-group-addon validate"><i></i></span>                                                    
+                                            </div>
+                                            <meter max="4" id="password-strength-meter"></meter>
+                                            <p id="password-strength-text"></p>
+                                        </div>
+                                    </div>
+
+                                    <!-- Confirmar Clave -->
+                                    <div class="form-group">
+                                        <label class="col-sm-4 control-label required">Confirmar Clave:</label>  
+                                        <div class="col-sm-6">
+                                            <div class="input-group">
+                                                <input id="Usu_Pal_C" name="Usu_Pal_C" type="password" class="form-control" required />
+                                                <span class="input-group-addon validate"><i></i></span>
+                                            </div>
+                                        </div>
+                                    </div>                                       
+
+                                </fieldset>
+                                
+                                <div class="text-center">
+                                    <button type="button" class="btn btn-primary" id="btnGuardar">
+                                        <i class="glyphicon glyphicon-floppy-disk"></i> Guardar
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- administrador/VALIDACIONES/adm_val_usuarios_2.0.js
+++ administrador/VALIDACIONES/adm_val_usuarios_2.0.js
@@ -1,5 +1,5 @@
 $(function(){
-   
+
     var flag_pass = 1;
     var flag_existe = 1;
     var password = document.getElementById('Usu_Pal');
@@ -30,28 +30,45 @@ $(function(){
     
     $("#Usu_Pal").on("blur",function(){
         validatePass("Usu_Pal","Usu_Pal_C");
-     });
+    });
     
     $("#Usu_Pal_C").on("blur",function(){
         validatePass("Usu_Pal","Usu_Pal_C");
-     });
+    });
     
     /*
      * Valida Passwords
      */
-    function validatePass(Usu_Pal,Usu_Pal_C)
-    {
-        if( $('#'+Usu_Pal).val() !== $('#'+Usu_Pal_C).val())
-        {
-            $('#'+Usu_Pal).fieldValid(false,"No coinciden los caracteres");
-            $('#'+Usu_Pal_C).fieldValid(false,"No coinciden los caracteres");
-            $('#'+Usu_Pal_C).val("");            
-            flag_pass = 1;
+    function validatePass(Usu_Pal, Usu_Pal_C) {
+        var p1 = $('#' + Usu_Pal).val();
+        var p2 = $('#' + Usu_Pal_C).val();
+        var regex = /^[a-zA-Z0-9]+$/;
+
+        if (p1 === "" && p2 === "") {
+            $('#' + Usu_Pal).fieldValid("");
+            $('#' + Usu_Pal_C).fieldValid("");
+            return;
         }
-        else
-        {
-            $('#'+Usu_Pal).fieldValid(true);
-            $('#'+Usu_Pal_C).fieldValid(true);
+
+        if (p1.length < 8) {
+            $('#' + Usu_Pal).fieldValid(false, "Mínimo 8 caracteres");
+            flag_pass = 1;
+            return;
+        }
+
+        if (!regex.test(p1)) {
+            $('#' + Usu_Pal).fieldValid(false, "Solo se permiten letras y números");
+            flag_pass = 1;
+            return;
+        }
+
+        if (p1 !== p2) {
+            $('#' + Usu_Pal).fieldValid(true);
+            $('#' + Usu_Pal_C).fieldValid(false, "No coinciden las contraseñas");
+            flag_pass = 1;
+        } else {
+            $('#' + Usu_Pal).fieldValid(true);
+            $('#' + Usu_Pal_C).fieldValid(true);
             flag_pass = 0;
         }
     }
@@ -64,27 +81,21 @@ $(function(){
      * GUARDAR
      *********/
     $("#btnGuardar").on("click", function(e){
-       e.preventDefault();
-       if (flag_pass === 0 && flag_existe === 0)
-       {
-           $.createDialogConfirm("Est&aacute; seguro de realizar esta accion ?","",function(){
-               $.getDataJson("",{updatePass:true, Usu_Pal: $("#Usu_Pal").val()},function(res){
-               $("#Usu_Pal_c").val("");
-               $("#Usu_Pal_c").fieldValid("");
-               $("#Usu_Pal").val("");
-               $("#Usu_Pal").fieldValid("");
-               $("#Usu_Pal_C").val("");
-               $("#Usu_Pal_C").fieldValid("");
-               meter.value = "";
-               text.innerHTML = "";
-               $.alert(res.message);
-           });
-           });          
-       }
-       else
-       {
-           $.alert("Verificar password actual y nuevo password");
-       }
+        e.preventDefault();
+        validatePass("Usu_Pal", "Usu_Pal_C"); // Re-validar antes de guardar
+    
+        if (flag_pass === 0 && flag_existe === 0) {
+            $.createDialogConfirm("¿Está seguro de realizar esta acción?","",function(){
+                $.getDataJson("", {updatePass: true, Usu_Pal: $("#Usu_Pal").val()}, function(res){
+                    $("#Usu_Pal_c, #Usu_Pal, #Usu_Pal_C").val("").fieldValid("");
+                    meter.value = "";
+                    text.innerHTML = "";
+                    $.alert(res.message);
+                });
+            });          
+        } else {
+            $.alert("Por favor, verifique que la contraseña sea alfanumérica y que coincidan.");
+        }
     });
     
     var strength = {
@@ -92,23 +103,21 @@ $(function(){
         1: "Baja",
         2: "Debil",
         3: "Bueno",
-        4: "Excelente !"
-      }
+        4: "Excelente!!"
+    }
 
     password.addEventListener('input', function() {
-      var val = password.value;
-      var result = zxcvbn(val);
+        var val = password.value;
+        var result = zxcvbn(val);
 
-      // Update the password strength meter
-      meter.value = result.score;
+        // Update the password strength meter
+        meter.value = result.score;
 
-      // Update the text indicator
-      if (val !== "") {
-        text.innerHTML = "Dificultad: " + strength[result.score]; 
-      } else {
-        text.innerHTML = "";
-      }
+        // Update the text indicator
+        if (val !== "") {
+            text.innerHTML = "Dificultad: " + strength[result.score]; 
+        } else {
+            text.innerHTML = "";
+        }
     });
 });
-
-
