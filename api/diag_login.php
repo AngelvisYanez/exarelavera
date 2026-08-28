e74d61321904be3412081aa94629a89ffd4ff683
exacontable <exacontable@gmail.com>
2026-08-28 17:43:05 -0400

    Use direct mysqli_query in diag_login

--- api/diag_login.php
+++ api/diag_login.php
@@ -10,7 +10,7 @@ $datos = new MysqlDatos();
 
 // Actualizar Usu_Pal para 22600781 en todas las empresas a md5('123456')
 $md5_123456 = md5('123456');
-$datos->consultaSql("UPDATE usuarios SET Usu_Pal = '$md5_123456' WHERE Usu_Ced = '22600781'", $con);
+mysqli_query($con->conexion, "UPDATE usuarios SET Usu_Pal = '$md5_123456' WHERE Usu_Ced = '22600781'");
 
 // Consultar los registros actualizados de 22600781 en empresas Torres Carrion
 $updated = $datos->getArrayConsultaSql(
