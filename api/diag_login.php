ed649cb49883c7af98e94ac46517bab1332204df
exacontable <exacontable@gmail.com>
2026-08-29 12:43:50 -0400

    Test join components in exa db

--- api/diag_login.php
+++ api/diag_login.php
@@ -7,12 +7,15 @@ try {
     require_once __DIR__ . '/../classes/DataAPI.php';
 
     $api = new DataAPI('exa');
-    $rows = $api->query("SELECT Tok_Id, Tok_Hash, LENGTH(Tok_Hash) as len, Tok_Nombre FROM api_tokens");
-    $simpleRow = $api->query("SELECT * FROM api_tokens WHERE Tok_Id = 1");
+    
+    $tables = $api->query("SHOW TABLES LIKE '%data%'");
+    $emp620 = $api->query("SELECT * FROM empresas WHERE Emp_Cod = 620");
+    $joinTest = $api->query("SELECT t.Tok_Id, t.Tok_Hash FROM api_tokens t WHERE t.Tok_Hash = 'b9033e7d59064208c76838f3200653fdb0e8c454ed2e0e94df68586aaa98f6b9'");
 
     echo json_encode([
-        'rows' => $rows,
-        'simpleRow' => $simpleRow
+        'tables' => $tables,
+        'emp620' => $emp620,
+        'joinTest' => $joinTest
     ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 } catch (\Throwable $e) {
     http_response_code(500);
