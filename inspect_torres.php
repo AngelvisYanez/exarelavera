f5ca5f8d8dd612cccde30c1896e0673133bb95e2
exacontable <exacontable@gmail.com>
2026-08-29 13:52:00 -0400

    Test sentencias_log 1 on 22600781

--- inspect_torres.php
+++ inspect_torres.php
@@ -4,10 +4,16 @@ ini_set('display_errors', 1);
 
 require_once __DIR__ . '/DATA/MysqlConexion.php';
 require_once __DIR__ . '/DATA/MysqlDatos.php';
+require_once __DIR__ . '/administrador/LOGICA/adm_sql_login.php';
 
 $obMaster = new MysqlConexion();
 $obDatos = new MysqlDatos();
 
-$allEmp = $obDatos->getArrayConsultaSql("SELECT Emp_Cod, Emp_Nom, Emp_Ruc, Dat_Dis FROM empresas ORDER BY Emp_Cod ASC", $obMaster);
+$sql = sentencias_log(1, ['22600781']);
+$res = $obDatos->getArrayConsultaSql($sql, $obMaster);
 
-echo json_encode($allEmp, JSON_PRETTY_PRINT) . "\n";
+echo json_encode([
+    'sql' => $sql,
+    'total' => count($res),
+    'empresas' => $res
+], JSON_PRETTY_PRINT) . "\n";
