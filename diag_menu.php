45f4b04fe13d42c1d8f33b1d8b6c4c1edc8e6ce7
exacontable <exacontable@gmail.com>
2026-08-29 13:39:52 -0400

    Search user 22600781 across databases

--- diag_menu.php
+++ diag_menu.php
@@ -3,67 +3,40 @@ chdir(__DIR__ . '/administrador/FRONT');
 
 require_once __DIR__ . '/DATA/MysqlConexion.php';
 require_once __DIR__ . '/DATA/MysqlDatos.php';
-require_once __DIR__ . '/administrador/LOGICA/logica.php';
-require_once __DIR__ . '/administrador/LOGICA/adm_log_menu_tree.php';
-require_once __DIR__ . '/administrador/LOGICA/adm_sql_menu.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
-$ced = '22600781';
-$obMaster = new MysqlConexion(); // master exa
+$obMaster = new MysqlConexion(); // exa
 $obDatos = new MysqlDatos();
 
-// 1. Get user in master / empresas
-$empresas = $obDatos->getArrayConsultaSql("
+$masterUsers = $obDatos->getArrayConsultaSql("
     SELECT u.Usu_Cod, u.Usu_Ced, u.Suc_Cod, s.Emp_Cod, e.Emp_Nom, e.Dat_Dis 
     FROM usuarios u 
-    INNER JOIN sucursal s ON u.Suc_Cod = s.Suc_Cod 
-    INNER JOIN empresas e ON s.Emp_Cod = e.Emp_Cod 
-    WHERE u.Usu_Ced = '$ced'
+    LEFT JOIN sucursal s ON u.Suc_Cod = s.Suc_Cod 
+    LEFT JOIN empresas e ON s.Emp_Cod = e.Emp_Cod 
+    LIMIT 50
 ", $obMaster);
 
-$dbLocal = 'ecoparkmining';
-if (!empty($empresas[0]['Dat_Dis'])) {
-    $dbLocal = $empresas[0]['Dat_Dis'];
-}
-
-$obLocal = new MysqlConexion($dbLocal);
-
-// 2. Get user in local DB
-$userLocal = $obDatos->getRowConsultaSql("
-    SELECT u.Usu_Cod, u.Usu_Ced, u.Usu_Men, u.Usu_Tip, u.Suc_Cod, u.Prs_Cod 
+$obLocal = new MysqlConexion('ecoparkmining');
+$localUsers = $obDatos->getArrayConsultaSql("
+    SELECT u.Usu_Cod, u.Usu_Ced, u.Suc_Cod, p.Prs_Nom, p.Prs_Ape 
     FROM usuarios u 
-    WHERE u.Usu_Ced = '$ced'
+    LEFT JOIN persona p ON u.Prs_Cod = p.Prs_Cod 
+    LIMIT 50
 ", $obLocal);
 
-// 3. Get profiles in local DB
-$profiles = [];
-if ($userLocal) {
-    $profiles = $obDatos->getArrayConsultaSql("
-        SELECT up.Per_Cod, p.Per_Des 
-        FROM usuarperfi up 
-        LEFT JOIN perfiles p ON up.Per_Cod = p.Per_Cod 
-        WHERE up.Usu_Cod = " . (int)$userLocal['Usu_Cod'],
-        $obLocal
-    );
-}
+// Look for 22600781 specifically anywhere
+$searchMaster = $obDatos->getArrayConsultaSql("SELECT * FROM usuarios WHERE Usu_Ced LIKE '%22600781%' OR Usu_Cod LIKE '%22600781%'", $obMaster);
+$searchLocal = $obDatos->getArrayConsultaSql("SELECT * FROM usuarios WHERE Usu_Ced LIKE '%22600781%' OR Usu_Cod LIKE '%22600781%'", $obLocal);
 
-// 4. Test menu generation
-$lisPer = array_map(function($p) { return $p['Per_Cod']; }, $profiles);
-$menuTree = new Class_Sys_Menu();
-$htmlMenu = '';
-if (!empty($lisPer)) {
-    $menuObj = $menuTree->getMenuContainer2($lisPer, $obLocal);
-    $htmlMenu = $menuTree->menuToHtml(1, $menuObj, 'nav nav-list', '');
-}
+$searchPersonaMaster = $obDatos->getArrayConsultaSql("SELECT * FROM persona WHERE Prs_Ced LIKE '%22600781%'", $obMaster);
+$searchPersonaLocal = $obDatos->getArrayConsultaSql("SELECT * FROM persona WHERE Prs_Ced LIKE '%22600781%'", $obLocal);
 
 echo json_encode([
-    'cedula' => $ced,
-    'dbLocal' => $dbLocal,
-    'empresas' => $empresas,
-    'userLocal' => $userLocal,
-    'profiles' => $profiles,
-    'lisPer' => $lisPer,
-    'htmlMenuLength' => strlen($htmlMenu),
-    'htmlMenuSnippet' => substr($htmlMenu, 0, 500),
+    'searchMaster' => $searchMaster,
+    'searchLocal' => $searchLocal,
+    'searchPersonaMaster' => $searchPersonaMaster,
+    'searchPersonaLocal' => $searchPersonaLocal,
+    'masterUsers' => $masterUsers,
+    'localUsers' => $localUsers,
 ], JSON_PRETTY_PRINT);
